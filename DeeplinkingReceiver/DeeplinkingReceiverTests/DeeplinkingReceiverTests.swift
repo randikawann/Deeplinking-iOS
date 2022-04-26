a1e276b62d987a8b890e51698580e25529a5d0f3
randika wanninayaka <randika@arimaclanka.com>
2022-04-26 09:16:54 +0530

    basic deeplinking

---
+++ DeeplinkingReceiver/DeeplinkingReceiverTests/DeeplinkingReceiverTests.swift
@@ -0,0 +1,33 @@
+//
+//  DeeplinkingReceiverTests.swift
+//  DeeplinkingReceiverTests
+//
+//  Created by Randika Wanninayaka on 2022-04-26.
+//
+
+import XCTest
+@testable import DeeplinkingReceiver
+
+class DeeplinkingReceiverTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ DeeplinkingSender/DeeplinkingSender/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DeeplinkingSender
+//
+//  Created by Randika Wanninayaka on 2022-04-26.
+//
+
+import UIKit
+import StoreKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func openReceiver(_ sender: Any) {
+        guard let url = URL(string: "ranApp://") else{
+             return
+        }
+        if(UIApplication.shared.canOpenURL(url)){
+            // deeplinking
+            UIApplication.shared.open(url, options: [:], completionHandler:nil)
+        }else{
+            //upcell
+            let vc = SKStoreProductViewController()
+            vc.loadProduct(withParameters: [SKStoreProductParameterITunesItemIdentifier: NSNumber(586447913)], completionBlock: nil)
+                present(vc, animated: true, completion: nil)
+        }
+    }
+    
+}
+
